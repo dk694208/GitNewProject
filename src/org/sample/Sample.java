fdc4fc2af971f27158c449ec934dea1de53d405e
dk6924208 <dk6924208@gmail.com>
2022-05-02 21:47:24 +0530

    Sample Program in this sub pass fail

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,18 @@
+package org.sample;
+public class Sample {
+	  public static void main(String[] args) {
+	   int maths1=40, maths2=35;
+		  int tamil1=60, tamil2=50, science1=35, science2=70,social1=90, social2=60, eng1=40 , eng2=20;
+		  
+		  if(maths1>=35 && maths2>=35 && science1>=35 && science2>=35 && tamil1>=35 && tamil2>=35 && social1>=35 && social2>=35 && eng1>=35 && eng2>=35) {
+				  System.out.println("pass");
+				 
+			  }
+			  else {
+				  System.out.println("fail");
+			  }
+			 }
+}
+		  
+		  
+	  
